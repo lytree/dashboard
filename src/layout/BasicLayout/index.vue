35264173efe1f468d90d2b287c83a3ecbffa930f
lytree <hi.yangzh@outlook.com>
2022-12-20 19:36:29 +0800

    add layout

--- src/layout/BasicLayout/index.vue
+++ src/layout/BasicLayout/index.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="flex flex-col w-full h-full min-w-[1200px]">
         <header
-            class="flex-shrink-0 w-full box-border duration-300 ease-in-out fixed z-[1001] min-w-[1200px] h-[56px] pl-[0]">
+            class="flex-shrink-0 w-full box-border duration-300 ease-in-out fixed z-[1001] min-w-[1200px] h-[56px] pl-[200px]">
             header
         </header>
         <div
-            class="flex-shrink-0 w-full box-border duration-300 ease-in-out z-[999] fixed top-[56px] min-w-[1200px] h-[56px] pl-[0]">
+            class="flex-shrink-0 w-full box-border duration-300 ease-in-out z-[999] fixed top-[56px] min-w-[1200px] h-[56px] pl-[200px]">
             tab
         </div>
-        <aside class="fixed left-0 top-0 box-border h-full transition-all z-[1002] w-[200px] pt-0 ">
+        <aside class="fixed left-0 top-0 box-border h-full transition-all z-[1002] w-[200px]">
             aside
         </aside>
-        <main class="grow w-full box-border duration-300 ease-in-out overflow-hidden">
+        <main class="grow w-full box-border duration-300 ease-in-out overflow-hidden pt-[112px] pl-[200px]">
             content
         </main>
         <footer
