38e5a50ba738eeb78427f154eb8516feeba78958
lytree <hi.yangzh@outlook.com>
2022-12-20 19:48:26 +0800

    add layout

---
+++ src/store/modules/theme/index.ts
@@ -0,0 +1,4 @@
+import { defineStore } from 'pinia';
+export const useThemeStore = defineStore('theme-store', {
+        
+})
--- src/typings/theme.d.ts
+++ src/typings/theme.d.ts
@@ -14,20 +14,13 @@ declare namespace Theme {
         sider: Sider;
         /** 菜单样式 */
         menu: Menu;
-    }
-    /** 全局头部属性 */
-    interface GlobalHeaderProps {
-        /** 显示logo */
-        showLogo: boolean;
-        /** 显示头部菜单 */
-        showHeaderMenu: boolean;
-        /** 显示菜单折叠按钮 */
-        showMenuCollapse: boolean;
+        /** 底部样式 */
+        footer: Footer;
+        /** 页面样式 */
+        page: Page;
     }
     /** 侧边栏样式 */
     interface Sider {
-        /** 侧边栏反转色 */
-        inverted: boolean;
         /** 侧边栏宽度 */
         width: number;
         /** 侧边栏折叠时的宽度 */
@@ -39,22 +32,20 @@ declare namespace Theme {
         /** vertical-mix模式下侧边栏的子菜单的宽度 */
         mixChildMenuWidth: number;
     }
+    /** 底部样式 */
+    interface Footer {
+        /** 是否固定底部 */
+        fixed: boolean;
+        /** 底部高度 */
+        height: number;
+    }
+    /** 全局头部属性 */
+    interface GlobalHeaderProps {
+        /** 显示logo */
+        showLogo: boolean;
+        /** 显示头部菜单 */
+        showHeaderMenu: boolean;
+        /** 显示菜单折叠按钮 */
+        showMenuCollapse: boolean;
+    }
 }
-/** 菜单项配置 */
-type GlobalMenuOption = {
-    key: string;
-    label: string;
-    routeName: string;
-    routePath: string;
-    icon?: () => import('vue').VNodeChild;
-    children?: GlobalMenuOption[];
-};
-/** 面包屑 */
-type GlobalBreadcrumb = import('element-plus').DropdownOption & {
-    key: string;
-    label: string;
-    disabled: boolean;
-    routeName: string;
-    hasChildren: boolean;
-    children?: GlobalBreadcrumb[];
-};
